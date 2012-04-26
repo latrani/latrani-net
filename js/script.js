289294bc04d74dc1bdf14da6142ec3c4c6847e1f
Matt Thomas <matt+github@latrani.net>
2012-04-25 21:19:31 -0700

    Mobile site adaptiveness

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-/* Author: 
+// Fix iPad's tendency to scale up when rotating.  
+// Code from http://stackoverflow.com/questions/5434656/ipad-layout-scales-up-when-rotating-from-portrait-to-landcape
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+    var viewportmeta = document.querySelector('meta[name="viewport"]');
+    if (viewportmeta) {
+        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+        document.body.addEventListener('gesturestart', function () {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+        }, false);
+    }
+}
 
-*/
-
-
+$(function(){
+    $("#mobile-nav").change(function(){
+        var href = $(this).val();
+        if (href) {
+            window.location = href;            
+        }
+    }).val(window.location.pathname);
+});
 
 
 
